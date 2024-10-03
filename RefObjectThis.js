1980c43be5ca16b3383a28c9041988fab12461c0
Suyash Shukla <49817345+suyash101998@users.noreply.github.com>
2024-10-03 23:40:27 +0530

    Update RefObjectThis.js
    Fixing the this word

--- RefObjectThis.js
+++ RefObjectThis.js
@@ -10,3 +10,14 @@ console.log(myUser.ref.name);
 
 //Solution : the output will be empty as myUser will be --> {name : "Suyash" , ref : Window object}
 //Why because when you call the makeUser what is the parent object the p[arent object is global object i.e window
+//A follow up can be how to fix this to Print Suyash
+function makeUser() {
+  return {
+    name : "Suyash",
+    ref() {
+      return this;
+    }
+  };
+}
+const myUser = makeUser();
+console.log(myUser.ref().name); // Since we have wrapped the this keyword inside a function and now the parent object is the first return object whose property value for name is Suyash it is fixed.
